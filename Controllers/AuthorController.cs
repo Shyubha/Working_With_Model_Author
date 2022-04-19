4f0c71a5993cb812085896bd179ed3ed9ee53536
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-04-19 15:14:47 +0530

    Add project files.

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Working_With_Model_Author.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace Working_With_Model_Author.Models
+{
+    public class Author
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        [DisplayName("id")]
+        public int Id { get; set; }
+
+
+        [DisplayName("Name")]
+        [MinLength(2)]
+        [MaxLength(15)]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+      
+
+        [DisplayName("MobileNo")]
+        [MinLength(7)]
+        [MaxLength(10)]
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage ="Mobile no is required")]
+        public int MobileNo { get; set; }
+
+
+    }
+}
